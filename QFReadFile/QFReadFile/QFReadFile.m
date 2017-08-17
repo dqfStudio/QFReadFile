e5f15b9ec35eb6b6c73bc7810487c9648cca8dbe
dqf <897432098@qq.com>
2017-08-17 23:44:24 +0800

    调整部分文件

--- QFReadFile/QFReadFile/QFReadFile.m
+++ QFReadFile/QFReadFile/QFReadFile.m
@@ -10,22 +10,6 @@
 
 @implementation QFReadFile
 
-+ (void)file:(NSString *)path block:(void(^)(NSString *lineStr))callback {
-    
-    const char *filePath = [path UTF8String];
-    FILE *fp1;//定义文件流指针，用于打开读取的文件
-    char textStr[10241];//定义一个字符串数组，用于存储读取的字符
-    fp1 = fopen(filePath,"r");//只读方式打开文件a.txt
-    while(fgets(textStr,10240,fp1)!=NULL)//逐行读取fp1所指向文件中的内容到text中
-    {
-        NSString *lineStr = [NSString stringWithCString:textStr encoding:NSUTF8StringEncoding];
-        if (callback && lineStr) {
-            callback(lineStr);
-        }
-    }
-    fclose(fp1);//关闭文件a.txt，有打开就要有关闭
-}
-
 + (void)folderPath:(NSString *)path block:(void(^)(NSString *path))callback {
     [self folderPath:path filter:nil block:callback];
 }
@@ -40,6 +24,22 @@
             callback(path);
         }
     }
+}
+
++ (void)file:(NSString *)path block:(void(^)(NSString *lineStr))callback {
+    
+    const char *filePath = [path UTF8String];
+    FILE *fp1;//定义文件流指针，用于打开读取的文件
+    char textStr[10241];//定义一个字符串数组，用于存储读取的字符
+    fp1 = fopen(filePath,"r");//只读方式打开文件a.txt
+    while(fgets(textStr,10240,fp1)!=NULL)//逐行读取fp1所指向文件中的内容到text中
+    {
+        NSString *lineStr = [NSString stringWithCString:textStr encoding:NSUTF8StringEncoding];
+        if (callback && lineStr) {
+            callback(lineStr);
+        }
+    }
+    fclose(fp1);//关闭文件a.txt，有打开就要有关闭
 }
 
 + (NSArray *)getClassPathInArr:(NSArray *)arr filter:(NSString *)filter {
